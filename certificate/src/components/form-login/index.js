ab489345b6bb24539938a9c4ddb7ea623a3b1696
Rose Oyakawa <rose.oyakawa@gmail.com>
2020-03-21 23:00:26 -0300

    Adicionado o FormLogin

--- certificate/src/components/form-login/index.js
+++ certificate/src/components/form-login/index.js
@@ -1,10 +1,66 @@
-import React, { Component } from 'react'
+import React from 'react';
+import { Form, Input, Button } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
-function FormLogin(props) {
 
-	return (
-		<h1>Esta é a função que trabalha com um componente</h1>
-	);
-}
+const FormLogin = () => {
+  const onFinish = values => {
+    console.log('Received values of form: ', values);
+  };
+
+  //O article está sendo utilizado do tachyons para colocar na formatação dos campos e a centralização na tela e a sombra
+  return (
+	<article class="mw5 center bg-white shadow-5 br3 pa3 pa4-ns mv3 ba b--black-10">
+    <Form 
+      name="normal_login"
+      className="login-form"
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+    >
+      <Form.Item
+        name="email"
+        rules={[
+          {
+            required: true,
+            message: 'Favor, informar seu email!',
+          },
+        ]}
+      >
+        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Email" />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Favor, informar a senha!',
+          },
+        ]}
+      >
+        <Input
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="Senha"
+        />
+      </Form.Item>
+      <Form.Item>
+        <a className="login-form-forgot" href="">
+          Esqueci a senha
+        </a>
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" style={{background:'#C6255A', 'border-color': '#C6255A', 'margin-right':'15%'}} htmlType="submit" className="login-form-button">
+          Entrar
+        </Button>
+        <a href="">Registrar-se!</a>
+      </Form.Item>
+    </Form>
+	</article>
+	
+  );
+};
 
 export default FormLogin;
--- certificate/src/pages/signUp/index.js
+++ certificate/src/pages/signUp/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function SingUp(props) {
+function SignUp(props) {
 
 	return (
 		<h1>Esta é a função que trabalha com a pagina para o registro do usuario</h1>
 	);
 }
 
-export default SingUp;
+export default SignUp;
