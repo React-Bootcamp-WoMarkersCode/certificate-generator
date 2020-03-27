ffd2b55ed792d806da344b30b3d4ee936252b9f0
Diana-ops <dianaregina22@outlook.com.br>
2020-03-27 03:25:54 -0300

    Component Section

--- certificate/src/components/section/index.js
+++ certificate/src/components/section/index.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './style.css'
 
 function Section(props) {
 
 	return (
-		<h1>Esta é a função que trabalha com o componente Section</h1>
+		<div className="div-section">
+			<h1 className="h1-section">
+				<span className="span-3">
+				Suas alunas </span> e participantes <br/> 
+				precisam de um 
+				<span className="span-1">
+					certificado
+				</span>? 
+			</h1>
+			<h2 className="h2-section">
+				Siga nosso 
+				<span className="span-2">
+					passo a passo</span>. <br/> é 
+					<span className="span-2" >
+						rápido
+					</span> é <span className="span-2"> 
+						prático!
+					</span>
+			</h2>
+			<Link to="/sign-up">
+				<button className="button-section">
+					EU QUERO
+				</button>
+			</Link>
+		</div>
+
 	);
 }
 
