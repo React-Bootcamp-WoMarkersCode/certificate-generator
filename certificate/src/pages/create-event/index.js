f3a10fa99e2fdee5f5c128a9566b1aee6e0fa1e6
Diana-ops <dianaregina22@outlook.com.br>
2020-03-28 18:33:00 -0300

    Atualizando brach DianaRegina

--- certificate/src/pages/create-event/index.js
+++ certificate/src/pages/create-event/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import FormEvent from '../../components/form-event/index'
+import UploadImg from '../../components/upload-img/index'
+import Footer from '../../components/footer/index'
 
 function CreateEvent(props) {
 
 	return (
 		<FormEvent />
+		<UploadImg />
+		<Footer />
 	);
 }
 
