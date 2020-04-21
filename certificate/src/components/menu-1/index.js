05f07fbbfce3c4957d3db34b7fdff9f0f8aeb422
Diana-ops <dianaregina22@outlook.com.br>
2020-04-21 00:20:41 -0300

    Melhorando menu-1

--- certificate/src/components/menu-1/index.js
+++ certificate/src/components/menu-1/index.js
@@ -1,24 +1,18 @@
 import React from "react";
-import { Menu, Layout } from "antd";
 import { Link } from "react-router-dom";
 import logoWomakers from "../../assets/img/logo_texto_cinza.png";
 import "./style.css";
 
 function MenuComponent() {
   return (
-    <Layout>
-      <Menu className="menu-1" >
-        <div className="logoWomakers">
-          <Link to="/"><img src={logoWomakers} alt="logo" /></Link>
-        </div>
-        <Menu.Item key="2">
-          <Link to="/sign-up">Cadastro</Link>
-        </Menu.Item>
-        <Menu.Item key="3">
-          <Link to="/sign-in">Login</Link>
-        </Menu.Item>
-      </Menu>
-    </Layout>
+      <div className="menu-1" >
+          <Link to="/"><img className="logo-menu" src={logoWomakers} alt="logo" /></Link>
+
+          <ul className="menu-list">
+            <Link to="/sign-up"><li>Cadastro</li></Link>
+            <Link to="/sign-in"><li>Login</li></Link>
+          </ul>
+      </div>
   );
 }
 
